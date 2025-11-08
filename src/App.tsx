9b6064af7452a64ea371a77174dff60fd27a6489
MatLan8 <152216691+MatLan8@users.noreply.github.com>
2025-11-08 23:04:41 +0200

    Added components and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<ShoppingCart />} />
+        </Routes>
       </div>
     </>
   );
---
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CartCard from "../../components/CartCard/CartCard";
+
+function ShoppingCart() {
+  return (
+    <div>
+      <CartCard></CartCard>
+    </div>
+  );
+}
+
+export default ShoppingCart;
