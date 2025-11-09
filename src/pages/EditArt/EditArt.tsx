5f9951b18bd464eea78e68b3eac2595f24a3ce62
M35KA <arnas.penkauskas@gmail.com>
2025-11-09 17:50:32 +0200

    routing in edit views

--- src/pages/EditArt/EditArt.tsx
+++ src/pages/EditArt/EditArt.tsx
@@ -1,14 +1,31 @@
 import styles from "./EditArt.module.css";
-import { artworks } from "../../MockUpData/ArtworkMock";
 import ArtForm from "../../components/ArtForm/ArtForm";
+import type { Artwork } from "../../types/Artwork";
+import { useLocation } from "react-router-dom";
 
 function EditArt() {
+  const location = useLocation();
+  const artwork = location.state?.artwork as Artwork;
+
+  const handleUpdate = (updatedArtwork: Artwork) => {
+    console.log("Updated artwork:", updatedArtwork);
+  };
+
+  const handleDelete = (deletedArtwork: Artwork) => {
+    console.log("Delete artwork:", deletedArtwork);
+  };
+
+  if (!artwork) return <p>No artwork data provided!</p>;
   return (
     <div className={styles.Container}>
       <div className={styles.List}>
         <div className={styles.title}>Edit Art</div>
         <div className={styles.divider} />
-        <ArtForm initialData={artworks[0]}></ArtForm>
+        <ArtForm
+          initialData={artwork}
+          onSubmit={handleUpdate}
+          onDelete={handleDelete}
+        ></ArtForm>
       </div>
     </div>
   );
