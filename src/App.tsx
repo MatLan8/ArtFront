8c23f82a195b13d20030416455df6b6c957fa624
MatLan8 <152216691+MatLan8@users.noreply.github.com>
2025-11-09 18:05:52 +0200

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Gallery from "./pages/Gallery/Gallery";
 import Home from "./pages/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
-      </Routes>
+    <div>
+      <NavBar />
+      <main style={{ paddingTop: "80px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Gallery" element={<Gallery />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css";
+import { ShoppingBag } from "lucide-react";
+
+function NavBar() {
+  return (
+    <nav className={styles.nav}>
+      <Link to="/">Home</Link>
+      <Link to="/Gallery">Gallery</Link>
+      <Link to="/Login">Login</Link>
+      <Link to="/Register">Register</Link>
+      <Link to="/ShoppingCart">
+        <ShoppingBag size={20} />
+      </Link>
+    </nav>
+  );
+}
+
+export default NavBar;
