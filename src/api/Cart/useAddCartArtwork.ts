28ecc29a9cf134d0c95efd29a0411d8d06cf2e9b
MatLan8 <152216691+MatLan8@users.noreply.github.com>
2025-12-12 17:51:52 +0200

    Setup api hooks

---
+++ src/api/Cart/useAddCartArtwork.ts
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+export interface CartArtworkCreate {
+  ClientId: number;
+  ArtworkId: number;
+  Count: number;
+  Price: number;
+}
+
+export const useAddCartArtwork = () => {
+  return useMutation<void, Error, CartArtworkCreate>({
+    mutationFn: async (packageC: CartArtworkCreate) => {
+      const { data } = await axios.post<void>(
+        `${import.meta.env.VITE_BASE_URL}/Cart/AddCartArtwork`,
+        packageC
+      );
+      return data;
+    },
+  });
+};
